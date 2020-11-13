e67e4806eb307eaf0f932d4312c8a98bc8b7843d
Paulo <paulituvitu@hotmail.com>
2020-11-13 16:38:48 -0300

    Frontend post search

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -1,11 +1,38 @@
 const postsContainer = document.querySelector('.posts-list .container');
 
-let likeButton = document.querySelector('.posts-list .container .card .post-info .likes button');
+const searchButton = document.querySelector('.search .container button');
+const searchInput = document.querySelector('.search .container input');
 
 function likePost(id) {
   fetch(`http://localhost:3333/like/${id}`, {
     method: 'PATCH',
   }).then(window.location.replace("index.html"));
+}
+
+async function searchPosts() {
+  const searchValue = searchInput.value;
+
+  const response = await fetch(`http://localhost:3333/posts?search=${searchValue}`);
+  const filteredPosts = await response.json();
+  
+  let template = '';
+
+  filteredPosts.forEach(post => {
+    template += 
+   `<div class="card">
+      <h2>${post.title}</h2>
+      <p>${post.content.slice(0, 200)}...</p>
+      <div class="post-info flex">
+        <a href="details.html?id=${post.id}" class="btn btn-dark">Ler mais...</a>
+        <div class="likes">
+          <span class="likes-number">${post.likes} Likes</span>
+          <button onclick="likePost(${post.id})"><i class="fas fa-thumbs-up"></i></button>
+        </div>
+      </div>
+    </div>`
+  });
+
+  postsContainer.innerHTML = template;
 }
 
 async function fetchPosts() {
@@ -33,4 +60,4 @@ async function fetchPosts() {
 }
 
 window.addEventListener('DOMContentLoaded', fetchPosts);
-likeButton.addEventListener('click', likePost);
+searchButton.addEventListener('click', searchPosts);
